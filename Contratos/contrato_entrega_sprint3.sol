1e3bb0cdb4dc95a29acc7453233656964524fefb
MaluMaiaa <99206636+MaluMaiaa@users.noreply.github.com>
2023-03-12 16:14:42 -0300

    Comentários do código

--- Contratos/contrato_entrega_sprint3.sol
+++ Contratos/contrato_entrega_sprint3.sol
@@ -4,79 +4,87 @@ pragma solidity ^0.8.0;
 contract cooverContract{
 
     //Declarando as variáveis utilizadas no código.
+
     address dono; //Dono do contrato
-    mapping (address => uint) public partValor; //Relaciona a carteira com o saldo
-    mapping (address => uint) public partImei;
-    bool public partContrato; 
-    uint public usuarios; //Usado para verificar a quantidade de pessoas dentro do contrato
+    mapping (address => uint) public partValor; // Relaciona a carteira com o saldo
+    mapping (address => uint) public partImei; // Relaciona a carteira com o IMEI do celular
+    bool public partContrato; // Verifica se o contrato está ativo ou inativo
+    uint public usuarios; // Usado para verificar a quantidade de pessoas dentro do contrato
     uint public saldoContrato; // Saldo do contrato
-    uint public totalContrato; //Total dentro do contrato
-    //Garante que o msg sender é o dono do contrato antes de realizar as funções
+    uint public totalContrato; // Total dentro do contrato
+
+    // Garante que o msg sender é o dono do contrato antes de realizar as funções
     modifier msgsender(){
         require(msg.sender == dono); _;
     }
 
-    //Constructor é uma espécie de função que roda apenas no deploy do contrato
+    // Constructor é uma espécie de função que roda apenas no deploy do contrato
     constructor( address[] memory integrantes, uint[] memory imei ) {
         for(uint i = 0; i<integrantes.length; i++){
-            partImei[integrantes[i]] = imei[i];
+            partImei[integrantes[i]] = imei[i]; // Adiciona o IMEI para cada participante
         }
-        dono = msg.sender;
-        partContrato = true;
+        dono = msg.sender; // Atribui o msg sender como o dono do contrato
+        partContrato = true; // Ativa o contrato
     }
 
 
-    //Função responsável pela entrada de um participante no contrato
+    // Função responsável pela entrada ($$) de um participante no contrato
+    
     function pagamentoInicial() public payable{
-        partValor[msg.sender] += msg.value;      
+        partValor[msg.sender] += msg.value; // Adiciona o valor ao saldo do participante      
     }
 
-    //Utilizada para adicionar um usuário a lista númerica da quantidade de usuário dentro do contrato.
+    // Função para adicionar um usuário a lista númerica da quantidade de usuário dentro do contrato
+    
     function entrar() public payable {
-        require(partContrato == true, "Contrato inativo");
-        require(partImei[msg.sender] == 0, "Ja participante");
-        partImei[msg.sender] = msg.value;
-        usuarios += 1;
+        require(partContrato == true, "Contrato inativo"); // Verifica se o contrato está ativo
+        require(partImei[msg.sender] == 0, "Ja participante"); // Verifica se o participante já está na lista
+        partImei[msg.sender] = msg.value; // Adiciona o valor do pagamento como IMEI do celular
+        usuarios += 1; // Adiciona um usuário
     }
 
-    //Responsável por verificar o saldo de uma carteira
+    // Responsável por verificar o saldo de uma carteira
+
     function obterSaldo() public view returns (uint) {
-        return partValor[msg.sender];
+        return partValor[msg.sender]; // Retorna o saldo
     }
 
-   //Função responsável por repor a reserva financeira do contato 
+   // Função responsável por repor a reserva financeira do contato 
+   
     function reposicaoReserva(address usuario) public payable{
-        require(totalContrato == 0, "A reserva do contrato precisa ser reposta");
-        saldoContrato = partValor[usuario];
-        totalContrato = msg.value;
+        require(totalContrato == 0, "A reserva do contrato precisa ser reposta"); // Verifica se a reserva está com valor igual a 0 
+        saldoContrato = partValor[usuario]; // Atribui o valor da carteira do usuário como saldo do contrato
+        totalContrato = msg.value; // Atribui o valor da mensagem como total do contrato
 
     }
 
-    //Mostra o saldo contido dentro do contrato
+    // Mostra o saldo contido dentro do contrato
+    
     function saldo_Contrato() public view returns (uint) {
-        return address(this).balance; 
+        return address(this).balance; // Retorna o saldo do contrato
     }
 
-    // mudança de saldo de acordo com a indenização.
-    function modificarSaldo(address _mudanca, uint _value, string memory oper) public msgsender {
-        require(keccak256(bytes(oper)) == keccak256(bytes("sub")) || keccak256(bytes(oper)) == keccak256(bytes("plus")), "so sub ou plus");
+    // Mudança de saldo de acordo com a indenização
 
-        if (keccak256(bytes(oper)) == keccak256(bytes("sub"))) {
+    function modificarSaldo(address _mudanca, uint _value, string memory oper) public msgsender {
+        require(keccak256(bytes(oper)) == keccak256(bytes("sub")) || keccak256(bytes(oper)) == keccak256(bytes("plus")), "so sub ou plus"); // Verifica se a operação a ser realizada é uma subtração ou adição
+        if (keccak256(bytes(oper)) == keccak256(bytes("sub"))) { // Caso a operação seja uma subtração
             require(partImei[_mudanca] >= _value, "Sem saldo suficiente");
             partImei[_mudanca] -= _value;
             payable(dono).transfer(_value);
 
-        } else if (keccak256(bytes(oper)) == keccak256(bytes("plus"))) {
+        } else if (keccak256(bytes(oper)) == keccak256(bytes("plus"))) { // Caso a operação seja uma adição
             partImei[_mudanca] += _value;
         }
     }
 
-    //Função responsável pela ação de solicitar indenização.
+    //Função para que o usuário peça uma indenização
+
     function pedirIndenizacao(uint _value) public {
-        require(partContrato == true, "Contrato inativo");
-            partImei[msg.sender] -= _value;
-            totalContrato -= _value;
-            payable(msg.sender).transfer(_value);
+        require(partContrato == true, "Contrato inativo"); // Verifica se o contrato está ativo
+            partImei[msg.sender] -= _value; // Verifica o IMEI 
+            totalContrato -= _value; // Verifica o valor total do contrato
+            payable(msg.sender).transfer(_value); // Transfere o valor
 }
 
 }
--- src/Deploy Ganache/contracts/Coover.sol
+++ src/Deploy Ganache/contracts/Coover.sol
@@ -3,42 +3,45 @@ pragma solidity ^0.8.0;
 
 contract cooverContract{
  
-    address owner;//Endereço do dono do contrato
-    uint public monthly; //Valor mensal do seguro
-    uint public users; //Numero total de adeptos ao contrato
-    bool public isActive; //Contrato está ativo ou inativo (true e false)
-    uint public coverage; //Valor de cobertura do objeto assegurado
-    uint public balance; //Saldo da carteira 
-    mapping (address => uint) public participants; //endereço dos usuarios e o preço pago por eles
+    address owner; // Endereço do dono do contrato
+    uint public monthly; // Valor mensal do seguro
+    uint public users; // Número total de adeptos ao contrato
+    bool public isActive; // Contrato está ativo ou inativo (true e false)
+    uint public coverage; // Valor de cobertura do objeto assegurado
+    uint public balance; // Saldo da carteira 
+    mapping (address => uint) public participants; // Endereço dos usuarios e o preço pago por eles
 
-    //Constructor é uma função que roda apenas uma vez na hora do deploy
+// Constructor é uma função que roda apenas uma vez na hora do deploy
     constructor(){
-        users = 0;
+        users = 0; 
         isActive = true;
-        owner = msg.sender; //Garante que aquele que fez o deploy é o dono do contrato
+        owner = msg.sender; // Garante que aquele que fez o deploy é o dono do contrato
     }
-
+    
+// Permite que os usuários participem do contrato de seguro pagando o valor mensal
     function join() public payable {
-    require(msg.value == monthly, "O valor do pagamento esta errado");
-    require(isActive == true, "O contrato nao esta mais ativo");
-    require(participants[msg.sender] == 0, "Este endereco ja participa do contrato");
-    participants[msg.sender] = msg.value;
-    users += 1;
+    require(msg.value == monthly, "O valor do pagamento esta errado"); // Verifica se o valor enviado é igual ao valor mensal
+    require(isActive == true, "O contrato nao esta mais ativo"); // Verifica se o contrato está ativo
+    require(participants[msg.sender] == 0, "Este endereco ja participa do contrato"); // Verifica se o endereço do usuário ainda não está participando do contrato
+    participants[msg.sender] = msg.value; // Adiciona o endereço do usuário e o valor pago
+    users += 1; // Adiciona um usuário 
 }
 
+// Permite que o proprietário do contrato ajuste o saldo de um usuário
     function changeBalance(address _mudanca, uint _value, string memory oper) public {
-        require(msg.sender == owner, "Not owner");
-        require(keccak256(bytes(oper)) == keccak256(bytes("sub")) || keccak256(bytes(oper)) == keccak256(bytes("plus")), "sob or plus");
-        if (keccak256(bytes(oper)) == keccak256(bytes("sub"))) {
+        require(msg.sender == owner, "Not owner"); // Verifica se o remetente da transação é o dono do contrato
+        require(keccak256(bytes(oper)) == keccak256(bytes("sub")) || keccak256(bytes(oper)) == keccak256(bytes("plus")), "sub or plus"); // Verifica se a operação a ser realizada é uma subtração ou adição
+        if (keccak256(bytes(oper)) == keccak256(bytes("sub"))) { // Caso a operação seja uma subtração
             participants[_mudanca] -= _value;
-        } else if (keccak256(bytes(oper)) == keccak256(bytes("plus"))) {
+        } else if (keccak256(bytes(oper)) == keccak256(bytes("plus"))) { // Caso a operação seja uma adição
             participants[_mudanca] += _value;
         }
-         participants[_mudanca] = 0;
+         participants[_mudanca] = 0; // Define o saldo do usuário como zero
          
     }
     
+// Retorna o saldo atual do contrato
     function getBalance() public view returns (uint){
-        return address(this).balance;
+        return address(this).balance; 
     }
 }
