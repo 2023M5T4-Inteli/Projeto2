cb00ad0b9b9d6e81ab14e0a3fdfdbc64f5a65375
MaluMaiaa <99206636+MaluMaiaa@users.noreply.github.com>
2023-03-15 16:48:03 -0300

    Front End - Dashboard (incompleto)

---
+++ src/Frontend/js/Usuário/Dashboard/dashboardUser.js
@@ -0,0 +1,25 @@
+//
+mudancaCards();
+// pega o elemento select
+var select = document.getElementById("selecao");
+
+// adiciona um evento onchange ao select
+select.onchange = function() {
+  mudancaCards(select.value)
+}
+
+  // pega o valor selecionado
+function mudancaCards(valor){
+  // verifica se o valor selecionado é opcao2
+  if (valor == "Grupo 1") {
+    // mostra os campos e o botão
+    document.getElementById("informacoesGrupo").style.display = "flex";
+    document.getElementById("botao").style.display = "block";
+  } else {
+    // oculta os campos e o botão
+    document.getElementById("informacoesGrupo").style.display = "none";
+    document.getElementById("botao").style.display = "none";
+    
+  }
+}
+
