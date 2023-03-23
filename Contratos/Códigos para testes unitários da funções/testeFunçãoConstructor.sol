340dbc0de458168d403daff5a3a6fd42a9a8ee96
EricTach <eric.tachdjian@sou.inteli.edu.br>
2023-03-23 17:18:29 -0300

    Preparação para a adição de mais testes

--- Contratos/Códigos para testes unitários da funções/testeFunçãoConstructor.sol
+++ Contratos/Códigos para testes unitários da funções/testeFunçãoConstructor.sol
@@ -4,28 +4,28 @@ pragma solidity ^0.8.0;
 import "remix_tests.sol";
 import "contracts/cooverContract.sol";
 
-contract cooverContractTest {
+contract constructorTest {
+
+cooverContract contrato;
+address[]  integrantes;
+uint[]  imei;
+
+function beforeAll() public {
+    integrantes = new address[](3);
+    integrantes[0] = address(this);
+    integrantes[1] = address(0x123);
+    integrantes[2] = address(0x456);
     
-    cooverContract contrato;
-    address[]  integrantes;
-    uint[]  imei;
+    imei = new uint[](3);
+    imei[0] = 12345;
+    imei[1] = 67890;
+    imei[2] = 24680;
     
-    function beforeAll() public {
-        integrantes = new address[](3);
-        integrantes[0] = address(this);
-        integrantes[1] = address(0x123);
-        integrantes[2] = address(0x456);
-        
-        imei = new uint[](3);
-        imei[0] = 12345;
-        imei[1] = 67890;
-        imei[2] = 24680;
-        
-        contrato = new cooverContract(integrantes, imei);
-    }
-    
-    function testConstructor() public {
-        //Verifica se o contrato foi criado com sucesso
-        Assert.equal(contrato.partContrato(), true, "Contrato nao criado");
-    }
+    contrato = new cooverContract(integrantes, imei);
+}
+
+function testConstructor() public {
+    //Verifica se o contrato foi criado com sucesso
+    Assert.equal(contrato.partContrato(), true, "Contrato nao criado com sucesso");
+}
 }
