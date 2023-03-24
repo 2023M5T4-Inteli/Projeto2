f648fb7c29bceeb1a847deb7e91cad4c4bb4ca1b
MathFidelis <99191536+MathFidelis@users.noreply.github.com>
2023-03-24 00:55:37 -0300

    casos de teste  unitarios  comentados
    comentei os casos testes

--- Contratos/Códigos para testes unitários da funções/testeFunçãoConstructor.sol
+++ Contratos/Códigos para testes unitários da funções/testeFunçãoConstructor.sol
@@ -1,31 +1,31 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.0; // Definindo a versão do Solidity e a licença do contrato
 
-import "remix_tests.sol";
-import "contracts/cooverContract.sol";
+import "remix_tests.sol"; // Importando a biblioteca Remix Tests
+import "contracts/cooverContract.sol"; // Importando o contrato cooverContract
 
-contract constructorTest {
+contract constructorTest { // Define um novo contrato chamado constructorTest
 
-cooverContract contrato;
-address[]  integrantes;
-uint[]  imei;
+cooverContract contrato; // Declara uma variável do tipo cooverContract
+address[]  integrantes; // Declara um array de endereços chamado integrantes
+uint[]  imei; // Declara um array de uint chamado imei
 
-function beforeAll() public {
-    integrantes = new address[](3);
-    integrantes[0] = address(this);
-    integrantes[1] = address(0x123);
-    integrantes[2] = address(0x456);
+function beforeAll() public { // Define uma função chamada beforeAll, que será executada antes de todos os testes
+    integrantes = new address[](3); // Inicializa o array integrantes com 3 elementos
+    integrantes[0] = address(this); // Define o primeiro elemento do array como o endereço do contrato atual
+    integrantes[1] = address(0x123); // Define o segundo elemento do array como o endereço 0x123
+    integrantes[2] = address(0x456); // Define o terceiro elemento do array como o endereço 0x456
     
-    imei = new uint[](3);
-    imei[0] = 12345;
-    imei[1] = 67890;
-    imei[2] = 24680;
+    imei = new uint[](3); // Inicializa o array imei com 3 elementos
+    imei[0] = 12345; // Define o primeiro elemento do array como 12345
+    imei[1] = 67890; // Define o segundo elemento do array como 67890
+    imei[2] = 24680; // Define o terceiro elemento do array como 24680
     
-    contrato = new cooverContract(integrantes, imei);
+    contrato = new cooverContract(integrantes, imei); // Cria uma nova instância do contrato cooverContract passando o array integrantes e o array imei como argumentos e atribui à variável contrato
 }
 
-function testConstructor() public {
+function testConstructor() public { // Define uma função chamada testConstructor, que será usada para testar a criação do contrato
     //Verifica se o contrato foi criado com sucesso
-    Assert.equal(contrato.partContrato(), true, "Contrato nao criado com sucesso");
+    Assert.equal(contrato.partContrato(), true, "Contrato nao criado com sucesso"); // Chama a função partContrato do contrato cooverContract e verifica se o valor retornado é verdadeiro. Se não for, exibe a mensagem "Contrato nao criado com sucesso"
 }
 }
--- Contratos/Códigos para testes unitários da funções/testeFunçãosaldoContrato.sol
+++ Contratos/Códigos para testes unitários da funções/testeFunçãosaldoContrato.sol
@@ -1,24 +1,25 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.0;  // Define a versão do Solidity e a licença do contrato
 
-import "remix_tests.sol"; // Importa o módulo de teste do Remix
-import "contracts/cooverContract.sol";
+import "remix_tests.sol"; // Importando o módulo de teste do Remix
+import "contracts/cooverContract.sol"; // Importando  o contrato cooverContract
 
-contract cooverContractTest {
+contract cooverContractTest { // Definindo um novo contrato chamado cooverContractTest
+
     cooverContract contrato; // Instância do contrato a ser testado
     
-    function beforeAll() public {
-        address[] memory integrantes = new address[](2);
-        uint[] memory imei = new uint[](2);
-        integrantes[0] = address(this);
-        imei[0] = 123456789;
-        integrantes[1] = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
-        imei[1] = 987654321;
+    function beforeAll() public { // Definindo uma função chamada beforeAll, que será executada antes de todos os testes
+        address[] memory integrantes = new address[](2); // Declara um array de endereços chamado integrantes com 2 elementos
+        uint[] memory imei = new uint[](2);  // Declara um array de uint chamado imei com 2 elementos
+        integrantes[0] = address(this); // Define o primeiro elemento do array como o endereço do contrato atual
+        imei[0] = 123456789; // Define o primeiro elemento do array como 123456789
+        integrantes[1] = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4; // Define o segundo elemento do array como o endereço 
+        imei[1] = 987654321; // Define o segundo elemento do array como 987654321
         contrato = new cooverContract(integrantes, imei); // Cria uma nova instância do contrato
     }
     
-    function saldo_Contrato_Deve_Retornar_Zero() public {
+    function saldo_Contrato_Deve_Retornar_Zero() public { // Define uma função chamada saldo_Contrato_Deve_Retornar_Zero para testar a função saldo_Contrato do contrato cooverContract
         uint balance = contrato.saldo_Contrato(); // Chama a função saldo_Contrato() do contrato
-        Assert.equal(balance, 0, "O saldo do contrato deve ser zero"); // Verifica se o saldo do contrato é igual a zero
+        Assert.equal(balance, 0, "O saldo do contrato deve ser zero"); //  Compara o valor retornado pela função saldo_Contrato com zero e exibe a mensagem "O saldo do contrato deve ser zero" se a comparação falhar
     }
 }
