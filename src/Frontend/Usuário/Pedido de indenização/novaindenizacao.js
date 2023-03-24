dd087efa91e7bd1bb2dfd6efcf3da8c3c9320c37
Giovanna Furlan Torres <giovanna.torres@sou.inteli.edu.br>
2023-03-24 01:39:35 -0300

    Update indenizações

--- src/Frontend/Usuário/Pedido de indenização/novaindenizacao.js
+++ src/Frontend/Usuário/Pedido de indenização/novaindenizacao.js
@@ -1,26 +1,45 @@
-const solicitarSinistro = document.querySelector('#botaoSolicitar');
+function solicitarIndenizacao(){
+  // pega as informações digitadas pelo usuário
+  var grupo = document.getElementById("selecaoGrupo").value;
+  var data = document.querySelector(".inputDataOcorrido").value;
+  var ocorrido = document.getElementById("selecaoOcorrido").value;
+  var arquivo = document.getElementById("file").value;
+  
+  // salva as informações no localStorage
+  localStorage.setItem("grupoSelecionado", grupo);
+  localStorage.setItem("dataOcorrido", data);
+  localStorage.setItem("motivoOcorrido", ocorrido);
+  localStorage.setItem("arquivoProvas", arquivo);
 
-
-solicitarSinistro.addEventListener('click', () => {
+  let timerInterval
     Swal.fire({
-        title: 'Deseja sair do grupo?',
-        text: "Uma vez que pedir para sair, só poderá entrar novamente caso tenha vaga e seja aberta outra solicitação.",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#00C171',
-        cancelButtonColor: '#eb3737',
-        confirmButtonText: 'Confirmar',
-        cancelButtonText: 'Cancelar'
+    title: 'Processando solicitação!',
+    html: 'A solicitação de indeniização está sendo processada. Aguarde <b></b> milisegundos.',
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: () => {
+        Swal.showLoading()
+        const b = Swal.getHtmlContainer().querySelector('b')
+        timerInterval = setInterval(() => {
+        b.textContent = Swal.getTimerLeft()
+        }, 100)
+    },
+    willClose: () => {
+        clearInterval(timerInterval)
+    }
     }).then((result) => {
-        if (result.isConfirmed) {
-            Swal.fire({
-                icon: 'success',
-                title: 'Você acabou de sair do grupo.',
-                text: 'Suas informações não constam mais neste grupo, você será redirecionado a tela inicial, onde poderá entrar em um novo grupo e/ou sair da plataforma.',
-                showConfirmButton: false,
-                timer: 3000
-            })
-            console.log(nomeGrupo)
-        }
-      })
-})
+    if (result.dismiss === Swal.DismissReason.timer) {
+        Swal.fire({
+            title: 'Solicitação enviada!',
+            text: "Sua solicitação de indenização foi enviada com sucesso! Aguarde a resposta da seguradora acerca do aceite e / ou a transação em sua wallet.",
+            icon: 'sucess',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Ok'
+          }).then((result) => {
+            if (result.isConfirmed) {
+                window.location.href = "./pedidoIndenizacao.html";
+            }
+          })
+    }
+    })
+}
