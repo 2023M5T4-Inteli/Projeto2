ceb8dbd0ed45b0259d4d620c964ba04b60d44395
Giovanna Furlan Torres <giovanna.torres@sou.inteli.edu.br>
2023-03-28 13:00:41 -0300

    Update Backend
    Criação banco de dados e verificação do e-mail para login

---
+++ src/Backend/Usuário/login.js
@@ -0,0 +1,34 @@
+const cors = require('cors');
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.static("../../Frontend/"));
+
+app.post('/login', (req, res) => {
+    const email = req.body.email;
+    console.log(email)
+    const query = `SELECT * FROM CooverUsuarios WHERE Email = ?`; // Change the placeholder to '?'
+
+    db.get(query, [email], (err, row) => {
+      if (err) {
+        res.status(500).send(err.message);
+        console.log(err.message);
+      } else if (row) {
+        res.status(200).json({ mensagem: 'Usuário encontrado.' });
+      } else {
+        res.status(404).json({ mensagem: 'Usuário não encontrado.' });
+      }
+    });
+});
+
+const db = new sqlite3.Database('../BancoDeDadosCoover.db');
+
+const PORT = 3081;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
--- src/Frontend/Seguradora/Login/loginADM.js
+++ src/Frontend/Seguradora/Login/loginADM.js
@@ -1,8 +1,3 @@
-// Adiciona um evento de clique no elemento com id "botaoLogin"
-document.getElementById('botaoLogin').addEventListener('click', async () => {
-    window.location.href = './verificacao.html';
-});
-
 // Adiciona um evento de clique no elemento com id "voltar"
 document.getElementById('voltar').addEventListener('click', async () => {
     window.location.href = '../../index.html';
@@ -13,3 +8,30 @@ function voltarInicioPlataforma() {
     window.location.href = '../../index.html';
 }
 
+
+// Verificação de Login
+function fazerLogin() {
+    alert('Fazendo login...')
+    const email = document.getElementById('email').value;
+  
+    fetch('http://localhost:3081/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+    },
+      body: JSON.stringify({ email })
+    })
+    .then((response) => {
+      if (response.ok) {
+        window.location.href = './verificacao.html';
+      } else {
+        throw new Error('Erro ao fazer login.');
+      }
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+}
+
+
+  
