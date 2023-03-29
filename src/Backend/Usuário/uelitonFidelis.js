6ccfb620c0bb4e282f722fd1771aa85d82984a65
MaluMaiaa <99206636+MaluMaiaa@users.noreply.github.com>
2023-03-29 12:29:01 -0300

    Update uelitonFidelis.js

--- src/Backend/Usuário/uelitonFidelis.js
+++ src/Backend/Usuário/uelitonFidelis.js
@@ -1,15 +1,13 @@
+
 const cors = require('cors');
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
-
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 app.use(express.static("../../Frontend/"));
-
 const db = new sqlite3.Database('../BancoDeDadosCoover.db');
-
 // Rota para obter a quantidade de membros do grupo
 app.get('/quantidade_membros', (req, res) => {
   const sql = 'SELECT * FROM cooverDadosUsuario WHERE grupo = ?';
@@ -22,16 +20,11 @@ app.get('/quantidade_membros', (req, res) => {
     }
   });
 });
-
 const PORT = 3081;
-
-
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
-
 // Adiciona um evento de clique no elemento com id "grupoSeguro"
 document.getElementById('grupoSeguro').addEventListener('click', async () => {
   window.location.href = '../Criação de grupo/visualizacaoGrupo.html';
 });
-
